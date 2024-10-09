8fb33b908dafe811fc50a2f398b36960ec8968b2
stephanyemmitty <stephanyemmitty@gmail.com>
2024-10-09 06:58:29 +0100

    update bas

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const passport = require("passport");
 const app = express();
 
 const PORT = 4000;
-const DB_NAME = "JobsPlanet"
 
 // Passport middleware
 app.use(passport.initialize());
@@ -28,7 +27,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 // routes
-var testAPIRouter = require("./routes/testAPI");
+var testAPIRoutre = require("./routes/testAPI");
 var UserRouter = require("./routes/users.routes");
 var JobRouter = require("./routes/job.routes");
 var ApplicationRouter = require("./routes/application.routes");
@@ -40,7 +39,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 // To localhost
-mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useNewUrlParser: true });
+mongoose.connect(process.env.MongoDB, { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully !");
